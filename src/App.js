fc0119c23723793716112d4006c7f66cdc63b605
Matthew van Boheemen <Matthew.vanBoheemen@Gmail.com>
2022-01-23 16:45:31 +1300

    Accept text from keyboard and display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen 123!</h2>
+  var [correctWord, setCorrectWord] = useState("chimp");
+  var [guessLineWords, setGuessLineWords] = useState(Array(6).fill(""));
 
-      <div className="letter">A</div>
+  function HandleKeyPress(key) {
+    // Find where this goes
+    var updatedLines = guessLineWords.slice(0);
+
+    const currentLineIndex = GetCurrentLineIndex();
+
+    console.log(currentLineIndex);
+
+    updatedLines[currentLineIndex] = updatedLines[currentLineIndex] + key;
+
+    setGuessLineWords(updatedLines);
+  }
+
+  function GetCurrentLineIndex() {
+    let result = -1;
+
+    guessLineWords.forEach((line, index) => {
+      if (line.length < 5 && result === -1) {
+        result = index;
+      }
+    });
+
+    return result;
+  }
+
+  return (
+    <div className="App" onKeyPress={(e) => HandleKeyPress(e.key)} tabIndex="0">
+      <h1>Matt's Wordle</h1>
+
+      <div className="GuessLines">
+        {guessLineWords.map((word, index) => (
+          <GuessLine word={word} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function GuessLine(props) {
+  const { word } = props;
+
+  let array = Array(5);
+  for (let i = 0; i < 5; i++) {
+    if (word.length > i) {
+      array[i] = word.substring(i, i + 1);
+    } else {
+      array[i] = "";
+    }
+  }
+
+  return (
+    <div className="GuessLine">
+      {array.map((el, i) => (
+        <div className="Letter" key={i}>
+          {el}
+        </div>
+      ))}
     </div>
   );
 }
